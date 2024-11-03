03fa4049b2d04c5b8b7c53d2ec4bb4e1e03c8857
muhammad1928 <muhammadsn7@gmail.com>
2024-11-03 23:06:05 +0100

    tutorial 3

--- tutorial3-numpy-datatypes.py
+++ tutorial3-numpy-datatypes.py
@@ -1,2 +1,12 @@
 import numpy as np
 
+a = np.array([[1,2,3], [4, "Hello", 6], [7,8,9]]) # this is possible with ordinary python arrays but not with numpy
+
+b = np.array([[1,2,3], [4, 5, 6], [7,8,9]])
+print(a.dtype) # <U11 = string with less than or equal to 11 characters
+
+print(type(a[0][0])) # <class 'numpy.str_'> which means its a string not a number
+print(a[0][0].dtype) # <U1 = a string with less than 1 character
+
+print(type(b[0][0])) # <class 'numpy.int64'> which mens its a integer in array b
+
